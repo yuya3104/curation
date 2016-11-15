a750070959ed749abff4adc6eac774c5b98aae50
yuya <yuya.skd@gmail.com>
2016-11-15 15:12:21 +0000

    add sign sign up and user detail

--- curation/config/routes.rb
+++ curation/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+
+  resources :users
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +59,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
